15d50d87e53a4d02e3ff215f1dd5d3c78cac55f4
FrameMuse <minicablestone@gmail.com>
2019-11-04 22:06:51 +0400

    Update stringer.php

--- bot/traits/stringer.php
+++ bot/traits/stringer.php
@@ -6,6 +6,8 @@
  * Enter the path to folder where there are files connected to string has call-name and defined for.
  * 
  * Each file should be in JSON fromat.
+ * 
+ * Max. number of attachments is 2 
  */
 trait stringer
 {
@@ -15,14 +17,14 @@ trait stringer
         self::folder = $folder;
         self::readDirectory($folder);
     }
-    private static function readJSON($file_name, $isArray = false)
+    private static function readJSON(string $file_name, bool $isArray = false)
     {
         $file_path = self::folder . "/" . $file_name . ".json";
         $file_data = json_decode($file_path . ".json", $isArray);
         if (!$isArray) self::files[$file_name] = $file_data;
         return $file_data;
     }
-    private static function readDirectory($folder)
+    private static function readDirectory(string $folder)
     {
         $files_in_dir = scandir($folder);
         foreach ($files_in_dir as $file_name) {
@@ -32,17 +34,33 @@ trait stringer
             }
         }
     }
-    static function cat($which) : object
+    static function cat(string $which) : object
     {
         return self::files[$which];
     }
-    static function alter($file, array $changes)
+    static function alter(string $file, array $changes)
     {
         $reads = self::readJSON($file, true);
         $changes = array_merge($reads, $changes);
 
         $file_path = self::folder . "/" . $file . ".json";
         file_put_contents($file_path, $changes);
+
+        return $changes;
+    }
+    static function code(string $file, array $variables)
+    {
+        foreach ($variables as $var => $value) {
+            foreach (self::readJSON($file, true) as $string => $data) {
+                if (gettype($data) == "array")
+                    foreach ($data as $string2 => $data2) {
+                        $changes[$string][$string2] = str_replace($var, $value, $data2);
+                    }
+                else
+                    $changes[$string] = str_replace($var, $value, $data);
+            }
+        }
+        return $changes;
     }
 }
 
