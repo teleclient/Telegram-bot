4f781e136e1178caae56bdef1673c0e2c8052322
FrameMuse <minicablestone@gmail.com>
2019-11-05 01:40:38 +0400

    Update awaitings.php

--- bot/classes/awaitings.php
+++ bot/classes/awaitings.php
@@ -2,7 +2,36 @@
 
 class awaitings
 {
-    
+    public function __construct(Type $var = null) {
+        $functions = get_class_methods($this);
+        unset($functions[0]);
+        unset($functions[1]);
+        $this->functions = $functions;
+
+        $this->db = new DataBase();
+        $this->db->connect();
+        $this->db->ping();
+    }
+    public function pull(): array
+    {
+        $options = [];
+        foreach ($this->functions as $function) {
+            $options[$function] = call_user_func($function);
+        }
+        return $options;
+    }
+    private function user(int $user)
+    {
+        $this->db->ping();
+        $result = $this->db->query("SELECT * FROM users WHERE user = '$user'");
+        @$row = $result->fetch_assoc();
+        return [
+            'isNew' => $result->num_rows == 0,
+            'hasName' => @$row['phone'] == NULL,
+            'name' => @$row['phone'],
+            'isOpped' => @$row['extra'] == "opped",
+        ];
+    }
 }
 
 ?>
