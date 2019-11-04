aafec6c4d099080a37a663120108ec555328a9cd
FrameMuse <minicablestone@gmail.com>
2019-11-04 21:23:59 +0400

    Update stringer.php

--- bot/traits/stringer.php
+++ bot/traits/stringer.php
@@ -15,11 +15,11 @@ trait stringer
         self::folder = $folder;
         self::readDirectory($folder);
     }
-    private static function readJSON($file_name): object
+    private static function readJSON($file_name, $isArray = false)
     {
-        $file_path = self::folder . "/" . $file_name;
-        $file_data = json_decode($file_path.".json");
-        self::files[$file_name] = $file_data;
+        $file_path = self::folder . "/" . $file_name . ".json";
+        $file_data = json_decode($file_path . ".json", $isArray);
+        if (!$isArray) self::files[$file_name] = $file_data;
         return $file_data;
     }
     private static function readDirectory($folder)
@@ -35,6 +35,14 @@ trait stringer
     static function cat($which) : object
     {
         return self::files[$which];
+    }
+    static function alter($file, array $changes)
+    {
+        $reads = self::readJSON($file, true);
+        $changes = array_merge($reads, $changes);
+
+        $file_path = self::folder . "/" . $file . ".json";
+        file_put_contents($file_path, $changes);
     }
 }
 
