22a7c5c7f2a89e2af3828cbafe362a88fd85397a
FrameMuse <minicablestone@gmail.com>
2019-11-05 19:36:46 +0400

    Update launcher.php

--- bot/launcher.php
+++ bot/launcher.php
@@ -16,6 +16,28 @@ $settings = file_get_contents(APPNAME_BOT_DIR . "/settings.json");
 $settings = json_decode($settings, true);
 
 $settings['connection_settings']['all']['proxy'] = ObfuscatedStream::getName();
+$settings['connection_settings']['all']['proxy_extra'] = [
+    [
+        'address'  => '96.44.183.149',
+        'port'     =>  55225,
+    ],
+    [
+        'address'  => '96.44.133.110',
+        'port'     =>  58690,
+    ],
+    [
+        'address'  => '50.62.59.61',
+        'port'     =>  55625,
+    ],
+    [
+        'address'  => '208.97.31.229',
+        'port'     =>  53124,
+    ],
+    [
+        'address'  => '66.110.216.105',
+        'port'     =>  39431,
+    ],
+];
 
 include 'login_helper.php'; // may not start without this
 $MadelineProto = new \danog\MadelineProto\API('sessions/bot.session', $settings);
