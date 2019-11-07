5b80323fc4f163d5eae60ab4514ceb08ee668b9d
FrameMuse <minicablestone@gmail.com>
2019-11-07 21:37:37 +0400

    Update useful.php

--- public/useful.php
+++ public/useful.php
@@ -1,7 +1,12 @@
 <?php
 
-ini_set('display_errors', 1);
+ini_set('error_display', 1);
 error_reporting(E_ALL);
+
+define("APPNAME_BOT_DIR", __DIR__);
+
+define("off", false);
+define("on", true);
 
 /**
      * Pretty colourful version of print, prints text into console
@@ -98,11 +103,16 @@ class log
     {
         if ($text) print($text); else print($this->print);
     }
+
+    public function lnlog($text = false)
+    {
+        if ($text) print(PHP_EOL.$text.PHP_EOL); else print(PHP_EOL.$this->print.PHP_EOL);
+    }
 }
 /**
  * This function resembles JavaScript function 'console'
  */
-function console(string $text)
+function console(string $text) : object
 {
     return new log($text);
 }
@@ -110,11 +120,14 @@ function console(string $text)
 
 class useful
 {
+    public static $settings = [
+        "notice" => on,
+    ];
     public $print;
     public function __construct()
     {
-        #console("Hello, I'm constructed and ready to help you because I'm the most useful class ever!")
-        #->paint("BLACK", "LIGHTGRAY");
+        console("Hello, I'm constructed and ready to help you because I'm the most useful class ever!")
+        ->paint("BLACK", "LIGHTGRAY");
     }
     
     public function date_to_words(string $date, $lang = null, $letters = null) {
@@ -141,112 +154,8 @@ class useful
             "year" => $date["year"],
         ];
     }
-}
-
-$useful = new useful();
-
-class DataBase
-{
-    public $useful;
-    public $config;
-    private $DataBase;
-    private $config_file = (__DIR__)."/database.json"; // It's niether a straight path to file or just a file
-    private $ping_loops = 0;
-    
-    /**
-     * @param object|array|null $mysqli 
-     */
-    public function __construct($option = null)
+    public static function setUp(array $settings)
     {
-        switch (gettype($option)) {
-            case "object":
-                $this->DataBase = &$option;
-                break;
-            case "array":
-                $this->setSettings($option);
-                break;
-            case "string":
-                $this->config_file = &$option;
-                $this->getSettings();
-                break;
-            default:
-                $this->getSettings();
-                break;
-        }
-    }
-
-    public function getSettings() : array
-    {
-        if (file_exists($this->config_file)) {
-            $file = file_get_contents($this->config_file);
-            $config = json_decode($file, true);
-        } else throw new Exception("Error: config file doesn't exist in '{$this->config_file}'");
-        #console("DataBase settings were successfully recieved")->paint("WHITE", "GREEN");
-        return $this->config = $config;
-    }
-
-    public function setSettings(array $settings)
-    {
-        $this->config = $settings;
-        $data = json_encode($settings, JSON_PRETTY_PRINT);
-        #if (file_put_contents($this->config_file, $data)) console("DataBase settings were successfully set up")->paint("WHITE", "GREEN");
-    }
-
-    public function connect()
-    {
-        try {
-            $this->DataBase = new mysqli($this->config['hostname'], $this->config['username'], $this->config['password'], $this->config['basename']);
-            if ($this->DataBase->connect_errno) {
-                $error = "Failed to connect to MySQL: (" . $this->DataBase->connect_errno . ") " . $this->DataBase->connect_error;
-                #console($error)->paint("WHITE", "RED");
-            }
-        } catch (\Exception $e) {
-            #console($e->getMessage())->paint("WHITE", "RED");
-        }
-    }
-
-    public function reconnect()
-    {
-        if (!$this->DataBase || $this->DataBase->close()) {
-            #console("Reconnection...")->paint("BLACK", "LIGHTGRAY", true);
-            $this->connect();
-            #print "     ";
-            #if (!$this->DataBase->connect_errno) console("OK")->paint("BLACK", "LIGHTGRAY");
-        }
-        else return false;
-    }
-
-    public function query($query, $close = false)
-    {
-        try {
-            if (!$this->DataBase) $this->ping();
-            if ($query != false) $result = $this->DataBase->query($query);
-            if (isset($this->DataBase->errno) && $this->DataBase->errno) {
-                throw new Exception("Failed to send a query to MySQL: (" . $this->DataBase->errno . ") " . $this->DataBase->error);
-            }
-            if ($close) $this->DataBase->close(); else $this->ping();
-            if (isset($result)) return $result; else return false;
-        } catch (\Exception $e) {
-            #console($e->getMessage())->paint("WHITE", "RED");
-        }
-    }
-    
-    public function ping(int $loops = 1000)
-    {
-        if ($this->DataBase && $this->DataBase->ping()) {
-            #if ($this->ping_loops % 5 == 0) console("Connection pinged - OK")->paint("WHITE", "CYAN");
-            if ($this->ping_loops > $loops) {
-                $this->reconnect();
-                $this->ping_loops = 0;
-            }
-            $this->ping_loops++;
-            return true;
-        } else {
-            #console("Connection pinged - ERROR: ".$this->DataBase->error)->paint("WHITE", "RED");
-            $this->connect(); // here is not 'reconnect' because it is already disconnected
-            return false;
-        }
+        self::$settings = &$settings;
     }
 }
-
-?>
