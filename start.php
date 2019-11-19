6a0636943eb2be18a1181d45f61d11d62221b10c
FrameMuse <minicablestone@gmail.com>
2019-11-19 23:34:01 +0400

    come back

--- start.php
+++ start.php
@@ -2,6 +2,11 @@
 
 include 'useful.php';
 
-include 'bot.php';
+$status = strstr(shell_exec("service tor status"), "is running") == true;
+if ($status) {
+    if (in_array(\useful::getGeo(), ["Russia"])) {
+        shell_exec('proxychains php -r "include \'bot.php\';" > bot.log');
+    } else include 'bot.php';
+}
 
 ?>
