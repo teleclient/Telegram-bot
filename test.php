8f1ef98a04ab16e6bc1a43cfb8f906870535f798
Valery Zinchenko <minicablestone@gmail.com>
2020-07-01 23:07:55 +0400

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,35 @@
 <?php
 
-$time = microtime(true);
-print microtime(true) - $time;
+function aqua() {
+    $random_filled_array = [];
+    $array_values = [
+        "min" => 0,
+        "max" => 100,
+        "length" => 100,
+        "amount" => 20,
+    ];
+
+    for ($i = 0; $i < $array_values["length"]; $i++) {
+        $random_filled_array[] = mt_rand($array_values["min"], $array_values["max"]);
+    }
+
+    for ($i = 0; $i < $array_values["length"]; $i++)
+
+        for ($j = $array_values["length"] - 1; $j > $i; $j--) {
+
+            $first_number = $random_filled_array[$j];
+            $second_number = $random_filled_array[$j - 1];
+
+            if ($first_number > $second_number) {
+                $random_filled_array[$j] = $second_number;
+                $random_filled_array[$j - 1] = $first_number;
+            }
+
+        }
+
+    return array_slice($random_filled_array, 0, $array_values["amount"]);
+}
+
+print_r(aqua());
 
 ?>
